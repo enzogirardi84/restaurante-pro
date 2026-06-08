ae3780d7cc03fef79f60e730038ab3b256258015
enzogirardi84 <enzogirardi84@gmail.com>
2026-06-07 22:55:29 -0300

    fix: add legacy dir to sys.path for imports

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -7,6 +7,8 @@ legacy_dir = Path(__file__).parent / "restaurante programa"
 legacy_file = legacy_dir / "sistema_restaurante.py"
 
 os.chdir(str(legacy_dir))
+if str(legacy_dir) not in sys.path:
+    sys.path.insert(0, str(legacy_dir))
 
 with open(legacy_file, encoding="utf-8") as f:
     code = compile(f.read(), str(legacy_file), "exec")
